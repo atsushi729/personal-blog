2e88edb30d68b75c4e3cdcb9d61068fded3c6c24
Atsushi <atu729@icloud.com>
2026-04-30 00:03:37 +0900

    feat: add closures blog post and diagram component

--- src/components/blog/registry.ts
+++ src/components/blog/registry.ts
@@ -1,8 +1,10 @@
 import type { MDXComponents } from "mdx/types";
+import * as closures from "./closures";
 import * as pureFunction from "./pure_function";
 import * as treeTraversal1 from "./tree_traversal_1";
 
 export const blogComponentRegistry: Record<string, MDXComponents> = {
+  closures: closures,
   pure_function: pureFunction,
   tree_traversal_1: treeTraversal1,
 };
